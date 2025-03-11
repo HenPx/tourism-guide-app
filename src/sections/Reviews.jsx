24bc2f69bf0a048b541e3ff622abe93cf1afd7cb
phnyxx <henptra@gmail.com>
2025-03-11 21:07:21 +0800

    fix: make prev next on review button

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowLeft from "../assets/svg/ArrowLeft";
 import ArrowRight from "../assets/svg/ArrowRight";
+import review from "../assets/json/reviews.json"
+
+
+const [currentIndex, setCurrentIndex] = useState(0)
+
+const handlePrevReview = () =>{
+    if (currentIndex>0){
+        setCurrentIndex(currentIndex -1);
+    }
+};
+
+const handleNextReview = ()=> {
+    if (currentIndex< review.length-1){
+        setCurrentIndex(currentIndex +1);
+    }
+};
 
 export default function ClientReview(){
     return(
         <>
         <div className="relative flex justify-center items-center">
-            <div className="absolute -left-6  top-1/2 -translate-y-1/2 border border-[#3085A7] w-[60px] h-[60px] flex items-center justify-center rounded-full shadow-md cursor-pointer text-[#3085A7]">
+
+            <button onClick={handlePrevReview} className="absolute -left-6  top-1/2 -translate-y-1/2 border border-[#3085A7] w-[60px] h-[60px] flex items-center justify-center rounded-full shadow-md cursor-pointer text-[#3085A7] disable={currentIndex === 0}">
                 <ArrowLeft/>
-            </div>
+            </button>
+            { review.length > 0 (
+                <div className="flex flex-col items-center rounded-xl shadow-[0_2px_100px_rgba(0,0,0,0.15)] px-6 py-8 w-[300px] sm:w-[474px] text-center bg-white">
+                    <img src={review[currentIndex].photo} alt="default" className="w-[70px] rounded-full p-2 bg-gray-200" />
+                    <h4 className="font-bold text-lg">{review[currentIndex].usernme}</h4>
+                    <p className="text-[#A8A8A8] text-sm">{review[currentIndex]
+                        .Job}</p>
+                    <p>{review[currentIndex].star}</p>
+                    <p className="text-[#A8A8A8]">{review[currentIndex].comment}</p>
+                </div>
+            )}
 
-            <div className="flex flex-col items-center rounded-xl shadow-[0_2px_100px_rgba(0,0,0,0.15)] px-6 py-8 w-[300px] sm:w-[474px] text-center bg-white">
-                <img src="src/assets/profileReviews/default.png" alt="default" className="w-[70px] rounded-full p-2 bg-gray-200" />
-                <h4 className="font-bold text-lg">Irfan Rahmat</h4>
-                <p className="text-[#A8A8A8] text-sm">Traveller</p>
-                <p>⭐⭐⭐⭐⭐</p>
-                <p className="text-[#A8A8A8]">I love Travelo, this is the best place to buy ticket and help you find your dream holiday.</p>
-            </div>
-
-            <div className="absolute -right-6 top-1/2 -translate-y-1/2 bg-[#3085A7] w-[60px] h-[60px] flex items-center justify-center rounded-full shadow-md cursor-pointe text-white">
+            <button onClick={handleNextReview} className="absolute -right-6 top-1/2 -translate-y-1/2 bg-[#3085A7] w-[60px] h-[60px] flex items-center justify-center rounded-full shadow-md cursor-pointe text-white disable={currentIndex === review.length-1}">
                 <ArrowRight/>
-            </div>
+            </button>
         </div>
 
         </> 
